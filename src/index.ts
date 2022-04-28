133216e3666a5a26f139e182d45c86f89719e74b
like-me4 <andretanforever@gmail.com>
2022-04-28 17:35:25 +0300

    add default config to tsconfig and webpack

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import './module';
+
+console.log('hello world')
+
+// function component() {
+//   const element = document.createElement('div');
+//
+//   element.innerHTML = 'Hello webpack'
+//
+//   return element;
+// }
+//
+// document.body.appendChild(component());
