233bd0c1307ac8b61cd4564bf7af461adbd8b70c
like-me4 <andretanforever@gmail.com>
2022-05-03 18:52:00 +0300

    Finish project config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-import './module';
+import './assets/scss/index.scss';
 
-console.log('hello world')
-
-// function component() {
-//   const element = document.createElement('div');
-//
-//   element.innerHTML = 'Hello webpack'
-//
-//   return element;
-// }
-//
-// document.body.appendChild(component());
+console.log('Working!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,85 @@
 const path = require('path');
+const webpack = require('webpack');
+
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const filename = ext => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
+
+const tsLoaders = () => {
+  const loaders = ['ts-loader'];
+  if (isDev) {
+    loaders.push('tslint-loader');
+  }
+  return loaders;
+};
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  mode: "development",
+  mode: 'development',
   entry: './index.ts',
-  devtool: 'inline-source-map',
+  devtool: isDev ? 'source-map' : false,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
+    },
+  },
+  devServer: {
+    port: 3000,
+    compress: true,
+    hot: isDev,
+  },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: tsLoaders(),
         exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
     ],
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
   output: {
-    filename: 'bundle.js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon-192.png'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
